4e5f89aca9757f8562bc15902feef57903dd38fa
Vasily <vasily.komarov1997@gmail.com>
2017-04-05 23:41:41 +0500

    test

---
+++ project/django/bin/django-admin.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import sys
+sys.path = ['E:/project/'] + sys.path
+
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
