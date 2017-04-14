d2934aae16f908e517a58955cf1c5e51009c23aa
vasily.komarov1997@gmail.com <vasily.komarov1997@gmail.com>
2017-04-14 22:25:57 +0500

    удалил папку джанго

--- project/django/bin/django-admin.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys
-sys.path = ['E:/project/'] + sys.path
-
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
