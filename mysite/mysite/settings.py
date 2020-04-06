b12997f29dc7ad730be8183fd5ce6e40402fface
Eduard <eduard-isakov@bk.ru>
2020-04-06 14:46:21 +0500

    collected static

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "mysite/polls/static")
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login'
