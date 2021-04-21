0d029e25ca5f7620035195f18104226468ca758f
Eduard Isakov <eduard-isakov@bk.ru>
2021-04-21 12:31:26 +0500

    fix fields order on register page

--- mysite/account/forms.py
+++ mysite/account/forms.py
@@ -3,13 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Account
 
 class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(label=u'Имя', max_length=100)
-    last_name = forms.CharField(label=u'Фамилия', max_length=100)
-    middle_name = forms.CharField(label=u'Отчество', max_length=100)
-    email = forms.EmailField(label=u"Эл. почта", max_length=100)
+    # first_name = forms.CharField(label=u'Имя', max_length=100)
+    # last_name = forms.CharField(label=u'Фамилия', max_length=100)
+    # # middle_name = forms.CharField(label=u'Отчество', max_length=100)
+    # email = forms.EmailField(label=u"Эл. почта", max_length=100)
 
     class Meta:
         model = Account
         fields = ['username', 'email',
-                  'first_name', 'phone_number', "school", 'city',
+                  'first_name', 'last_name', 'middle_name',
+                  'phone_number', "school", 'city',
                   'password1', 'password2']
